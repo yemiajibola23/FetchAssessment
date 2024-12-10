309cc1713dade591525bf7c2b5845e70dcfe84f8
Yemi Ajibola <yajibola23@gmail.com>
2024-12-10 13:23:27 -0600

    Initial Commit

---
+++ Fetch Assessment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch Assessment
+//
+//  Created by Yemi Ajibola on 12/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch Assessment/Fetch_AssessmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_AssessmentApp.swift
+//  Fetch Assessment
+//
+//  Created by Yemi Ajibola on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_AssessmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
